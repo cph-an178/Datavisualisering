3aa0936894a247d35bb273a50c2e77b4e97a4345
Alexander Nilsen <cph-an178@cphbusiness.dk>
2018-12-20 10:41:26 +0100

    Started on activity months

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyodbc
+import datetime
 
 with open("sqlserver.txt", "r") as file:
     _arr = file.read().split(",")
@@ -7,8 +8,53 @@ with open("sqlserver.txt", "r") as file:
     user = _arr[2]
     passwd = _arr[3]
 
-conn = pyodbc.connect(r'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+user+';PWD='+passwd)
+conn = pyodbc.connect(r'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='
+                    +user+';PWD='+passwd)
 cursor = conn.cursor()
 
 supplies_table = cursor.execute("SELECT * FROM SC010100").fetchall()
 
+def create_date_amount_dict(search_number):
+    """
+        This function creates a dictionary of a givin product or
+        product group with the ordered date as key and the amount as the value
+    """
+    # Empty result dictionary
+    rs_dict = {}
+
+    # If it's an product number
+    if len(search_number) > 4:
+        prod_list = cursor.execute('SELECT CONVERT( nchar, OR03001) as ordernr,' 
+                            +'CONVERT(float, OR03011) as amount FROM OR030100 '
+                            +'WHERE OR03005 = CONVERT( nvarchar,' + search_number + ');').fetchall()
+
+        for i in prod_list:
+            od = cursor.execute('SELECT OR01002 as orderType, OR01015 as orderDate FROM OR010100 WHERE OR01001 = '
+                            + i[0] +';').fetchone()
+            if od != None:
+                date = od[1].date().strftime("%d-%m-%Y")
+                rs_dict.update({date:i[1]})
+
+    # If it's a product groupe
+    else: 
+        prod_cat = cursor.execute('SELECT CONVERT( nchar, SC01001) as artnr FROM SC010100 '
+                            +'WHERE SC01037  = CONVERT( nvarchar,' + search_number + ');').fetchall()
+        print("Product cat list : ", prod_cat[:1])
+        for i in prod_cat:
+            i[0] = i[0].strip()
+            prod_list = cursor.execute('SELECT CONVERT( nchar, OR03001) as ordernr,' 
+                            +'CONVERT(float, OR03011) as amount FROM OR030100 '
+                            +'WHERE OR03005 = CONVERT( nvarchar,' + i[0] + ');').fetchall()
+            print("Product list : ", prod_list)
+            for n in prod_list:
+                n[0] = n[0].strip()
+                print("Product : ", n)
+                
+                od = cursor.execute('SELECT OR01002 as orderType, OR01015 as orderDate FROM OR010100 WHERE OR01001 = '
+                            +'CONVERT( nvarchar, ' + n[0].strip() +');').fetchone()
+                if od != None:
+                    date = od[1].date().strftime("%d-%m-%Y")
+                    rs_dict.update({date:n[1]})
+    
+
+    return rs_dict
--- test_main.py
+++ test_main.py
@@ -1,3 +1,4 @@
+import main
 import pyodbc
 
 def test_conn():
@@ -7,8 +8,26 @@ def test_conn():
         database = _arr[1]
         user = _arr[2]
         passwd = _arr[3]
-
     conn = pyodbc.connect(r'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+user+';PWD='+passwd)
     re = conn.cursor().execute("SELECT * FROM SC010100").fetchone()
-    
+
     assert re != None
+
+def test_create_date_amount_product():
+    """
+        This test that we create an dictonary that adds a date as key
+        and a amount sold as a value
+    """
+    artnr = "342412" # Product number
+    date_amount_dict = main.create_date_amount_dict(artnr)
+    # We know that the dictionary's length should be larger than 0
+    assert len(date_amount_dict) != 0
+
+def test_create_date_amount_prod_groupe():
+    """
+        This test makes sure that our create_date_amount_dict function
+        can also create an dictionary of a product group
+    """
+    product_group = "101"
+    date_amount_dict = main.create_date_amount_dict(product_group)
+    assert len(date_amount_dict) != 0
