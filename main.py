7c7d005b9ae48cb355d86b103a40f9efd52a6f7e
Alexander Nilsen <cph-an178@cphbusiness.dk>
2019-01-02 16:04:57 +0100

    Removed dummy function and fixed variable name

--- main.py
+++ main.py
@@ -3,21 +3,16 @@ import pyodbc
 
 def connect_to_db(db_connection_file):
     with open(db_connection_file, "r") as file:
-        _arr = file.read().split(",")
-        server = _arr[0]
-        database = _arr[1]
-        user = _arr[2]
-        passwd = _arr[3]
+        _l = file.read().split(",")
+        server = _l[0]
+        database = _l[1]
+        user = _l[2]
+        passwd = _l[3]
     
     conn = pyodbc.connect(r'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='
                     +user+';PWD='+passwd)
 
     return conn.cursor()
-
-
-def dummy_def(cursor, sql_table, sql_row,search_num):
-    rs = cursor.execute("SELECT TOP 5 * FROM " + sql_table + " WHERE " + sql_row + " ='" + search_num + "'").fetchall()
-    return rs
 
 
 def main():
